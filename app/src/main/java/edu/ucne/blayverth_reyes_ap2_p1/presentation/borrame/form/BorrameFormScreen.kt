5725049cdf479e3a9c7559a44588a0d1940f7416
Blaiva <blayverthreyesp14@gmail.com>
2026-06-02 21:27:48 -0400

    Crea los screen del form y el list

---
+++ app/src/main/java/edu/ucne/blayverth_reyes_ap2_p1/presentation/borrame/form/BorrameFormScreen.kt
@@ -0,0 +1,13 @@
+package edu.ucne.blayverth_reyes_ap2_p1.presentation.borrame.form
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun BorrameFormScreen(){
+}
+
+@Preview
+@Composable
+private fun BorrameFormBodyPreview(){
+}
---
+++ app/src/main/java/edu/ucne/blayverth_reyes_ap2_p1/presentation/borrame/list/BorrameListScreen.kt
@@ -0,0 +1,13 @@
+package edu.ucne.blayverth_reyes_ap2_p1.presentation.borrame.list
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun BorrameListScreen(){
+}
+
+@Preview
+@Composable
+private fun BorrameListBodyPreview(){
+}
